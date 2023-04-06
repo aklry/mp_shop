96ca434cdc3e8c1e8a6ede9ca37196c08d0751a4
Aklry <74409739+aklry@users.noreply.github.com>
2023-04-06 19:42:13 +0800

    添加商品详情页面

---
+++ pages/details/details.js
@@ -0,0 +1,34 @@
+const { api } = require('../../api/index')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    goodsDetails: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    //提示用户正在获取数据
+    wx.showLoading({
+      title: '加载中'
+    })
+    api.getGoodsDetails({
+      id: options.id
+    }).then(res => {
+      if (res.data.status === 200) {
+        wx.hideLoading()
+        this.setData({
+          goodsDetails: res.data.data[0]
+        })
+      } else {
+        wx.showToast({
+          title: '获取数据失败',
+        })
+      }
+    }).catch(error => console.log(error))
+  }
+})
