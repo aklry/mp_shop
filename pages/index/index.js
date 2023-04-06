4546a2cde6b22a5b53c3c355c4df259d91cf9d69
Aklry <74409739+aklry@users.noreply.github.com>
2023-04-06 16:44:36 +0800

    使用商品列表组件渲染数据并实现上拉加载功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,6 @@
-const { getBanner } = require('../../api/index')
+const {
+  api
+} = require('../../api/index')
 Page({
   data: {
     value: '',
@@ -9,14 +11,80 @@ Page({
       duration: 1000,
       circular: true
     },
-    swiperData: []
+    swiperData: [],
+    navData: [{
+        text: '数码',
+        icon: 'like',
+        color: '#ff0000'
+      },
+      {
+        text: '生鲜',
+        icon: 'star',
+        color: '#ff0000'
+      },
+      {
+        text: '会员',
+        icon: 'fire',
+        color: '#ff0000'
+      },
+      {
+        text: '优惠',
+        icon: 'gift',
+        color: '#ff0000'
+      },
+      {
+        text: '充值',
+        icon: 'phone',
+        color: '#ff0000'
+      },
+      {
+        text: '领券',
+        icon: 'gem',
+        color: '#ff0000'
+      },
+      {
+        text: '外卖',
+        icon: 'gift-card',
+        color: '#ff0000'
+      },
+      {
+        text: '美食',
+        icon: 'smile',
+        color: '#ff0000'
+      },
+    ],
+    page: 1,
+    goodsData: []
   },
   onLoad() {
-    getBanner().then(res => {
+    api.getBanner().then(res => {
       // console.log(res.data.data.result)
       this.setData({
         swiperData: res.data.data.result
       })
     })
+    this.http(this.data.page)
   },
+  http(page) {
+    api.getGoods({
+      page
+    }).then(res => {
+      if (!res.data.msg) {
+        this.setData({
+          goodsData: this.data.goodsData.concat(res.data.data.result)
+        })
+      } else {
+        wx.showToast({
+          title: '暂无数据',
+        })
+      }
+    }).catch(error => console.log(error))
+  },
+  onReachBottom() {
+    //触底时更新页码
+    this.setData({
+      page: this.data.page += 1
+    })
+    this.http(this.data.page)
+  }
 })
