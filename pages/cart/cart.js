2526494fd15daaa3c4075ddf2e35b1b623b57e7a
Aklry <74409739+aklry@users.noreply.github.com>
2023-04-06 23:07:32 +0800

    实现删除购物车商品的功能

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -14,6 +14,34 @@ Page({
    * 生命周期函数--每次打开页面，都会加载
    */
   onShow(options) {
+    this.http()
+  },
+  /**
+   * 根据id删除商品
+   * 1.currentID:商品id
+   * 2.id:商品数据的唯一索引
+   * @param {*} e 
+   */
+  delCartHandle(e) {
+    const {
+      id
+    } = e.currentTarget.dataset
+    api.delCart({
+      currentID: id
+    }).then(res => {
+      if (res.data.status === 200) {
+        wx.showToast({
+          title: '删除成功',
+        })
+        this.http()
+      } else {
+        wx.showToast({
+          title: '删除失败',
+        })
+      }
+    }).catch(error => console.log(error))
+  },
+  http() {
     api.getCart().then(res => {
       if (res.data.status === 200) {
         this.setData({
