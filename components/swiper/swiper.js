c20a851296b828a6ee6470c03181ba8ce42180e1
Aklry <74409739+aklry@users.noreply.github.com>
2023-04-06 11:25:39 +0800

    封装轮播图组件

---
+++ components/swiper/swiper.js
@@ -0,0 +1,38 @@
+// components/swiper/swiper.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    indicatorDots: {
+      type: Boolean,
+      value: false
+    },
+    autoplay: {
+      type: Boolean,
+      value: false
+    },
+    inerval: {
+      type: Number,
+      value: 5000
+    },
+    duration: {
+      type: Number,
+      value: 500
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
