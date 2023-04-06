2e9594a0088e355de1bc9e5b1c4d81355e443ff4
Aklry <74409739+aklry@users.noreply.github.com>
2023-04-06 22:32:15 +0800

    实现购物车页面布局, 渲染购物车数据

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,66 +1,29 @@
-// pages/cart/cart.js
+const {
+  api
+} = require('../../api/index')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    cartData: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    api.getCart().then(res => {
+      if (res.data.status === 200) {
+        this.setData({
+          cartData: res.data.data
+        })
+      } else {
+        wx.showToast({
+          title: '获取数据失败',
+        })
+      }
+    }).catch(error => console.log(error))
   }
 })
