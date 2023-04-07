58aa5fc687da65d6abe361b71ee1b8cf03871752
Aklry <74409739+aklry@users.noreply.github.com>
2023-04-07 16:53:21 +0800

    实现登录功能

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,66 +1,51 @@
-// pages/user/user.js
+const { api } = require('../../api/index')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    if (wx.getStorageSync('userInfo')) {
+      this.setData({
+        userInfo: wx.getStorageSync('userInfo')
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  getUserProfile() {
+    wx.getUserProfile({
+      desc: '展示用户信息',
+      success: res => {
+        this.setData({
+          userInfo: res.userInfo
+        })
+        this.loginHandle()
+        wx.setStorageSync('userInfo',res.userInfo)
+      },
+      fail(error){
+        console.log(error)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  loginHandle() {
+    wx.login({
+      success: (res) => {
+        //code:发送给接口
+        api.login({
+          code: res.code
+        }).then(resp => {
+          wx.setStorageSync('loginID', resp.data.data)
+        }).catch(error => console.log(error))
+      },
+      fail(error){
+        console.log(error)
+      }
+    })
   }
 })
